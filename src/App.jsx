b05bd44408a80a47647402d5312bbe8ddf4c9337
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-05-04 12:35:02 +0100

    protext routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 
 import './App.css'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import GetStarted from './pages/GetStarted'
 import Maps from './pages/Maps'
 import UserDetails from './pages/UserDetails'
+import { useUser } from './context/UserContext'
 
 function App() {
-
+const {isLoggedIn } = useUser()
   return (
     <div className="">
         <Routes>
         <Route path='/' index element={<Home/>}></Route>
-        <Route path='/boarding' element={<GetStarted/>}></Route>
-        <Route path='/map' element={<Maps/>}></Route>
-        <Route path='/user/details' element={<UserDetails/>}></Route>
+        <Route path='/boarding' element={isLoggedIn ? <GetStarted/> : <Navigate to="/"/>}></Route>
+        <Route path='/map' element={isLoggedIn ? <Maps/>  : <Navigate to="/"/>}></Route>
+        <Route path='/user/details' element={isLoggedIn ? <UserDetails/> : <Navigate to="/"/>}></Route>
         </Routes>
     </div>
   )
